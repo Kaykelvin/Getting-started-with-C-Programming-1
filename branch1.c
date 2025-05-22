6863416c8b690569f34ca8fa9c55ade56af6c90c
kabalikelvin12345@gmail.com <kabalikelvin12345@gmail.com>
2025-05-22 17:57:20 +0300

    branch1: second commit

--- branch1.c
+++ branch1.c
@@ -1,16 +1,3 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int age = 20;
-    float height = 5.9;
-    double distance = 12345.6789;
-    char initial = 'A';
-
-    printf("Age: %d\n", age);
-    printf("Height: %.1f\n", height);
-    printf("Distance: %.4lf\n", distance);
-    printf("Initial: %c\n", initial);
-
-    return 0;
-}
